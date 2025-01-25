ffa3898b8911f7aa3a40a25f7a67086fcdd4ae82
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-01-25 18:16:14 +0800

    Remove unecessary import

--- src/main/java/com/thesis/phishing_detector/Bean/WebSocketHandlerImpl.java
+++ src/main/java/com/thesis/phishing_detector/Bean/WebSocketHandlerImpl.java
@@ -4,6 +4,7 @@ import com.thesis.phishing_detector.Services.UrlProcessingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -13,6 +14,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+@Component
 public class WebSocketHandlerImpl extends TextWebSocketHandler {
 
     private final Logger logger = LoggerFactory.getLogger(WebSocketHandlerImpl.class);
--- src/main/java/com/thesis/phishing_detector/Services/ApiService.java
+++ src/main/java/com/thesis/phishing_detector/Services/ApiService.java
@@ -1,8 +1,5 @@
 package com.thesis.phishing_detector.Services;
 
-
-import reactor.core.publisher.Mono;
-
 public interface ApiService {
     String analyzeUrl(String url);
 }
