3a3d37aaa2c5ac63fadf60cb2faf2a3ee6d14adf
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-01-30 13:46:02 +0800

    add get method

--- src/main/java/com/thesis/phishing_detector/Configuration/CORSConfig.java
+++ src/main/java/com/thesis/phishing_detector/Configuration/CORSConfig.java
@@ -11,7 +11,7 @@ public class CORSConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/check-url") // Apply to all endpoints
                 .allowedOrigins("https://mail.google.com","http://127.0.0.1:5500") // Allow specific origins
-                .allowedMethods("POST") // Allow specific HTTP methods
+                .allowedMethods("POST","GET") // Allow specific HTTP methods
                 .allowedHeaders("Client-Type", "Content-Type") // Allow all headers
                 .maxAge(3600); // Max age for preflight requests (in seconds)
     }
